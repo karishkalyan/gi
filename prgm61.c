800df2e25041feb5db7477033d17c0aeb0e46e69
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-23 16:11:03 +0530

    string
    prgm

---
+++ prgm61.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[i];
+int i,k;
+printf("\n enter the string:");
+scanf("%s",&a[i]);
+printf("\n enter the limit");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+printf("\n ",a[i]);
+}
+return0;
+}
